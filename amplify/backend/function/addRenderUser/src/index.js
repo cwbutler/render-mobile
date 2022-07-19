18c91e1e5719cd0e93cfe95fa98e5b42be7e0222
Christopher Butler <chris@project9.studio>
2022-07-19 17:49:48 -0400

    Added create users lambda

--- amplify/backend/function/addRenderUser/src/index.js
+++ amplify/backend/function/addRenderUser/src/index.js
@@ -3,10 +3,18 @@
 	REGION
 	AIRTABLE_API_KEY
 Amplify Params - DO NOT EDIT */
+const Airtable = require('airtable');
+const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base('appo8ITXRoiHVVPoe');
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
-exports.handler = async (event) => {
-    return event.arguments.input;
+exports.handler = async (event, _, callback) => {
+    try {
+        const result = await base('Users').create(event.arguments.input);
+        return result?.fields;
+    } catch (e) {
+        console.log(e.data.error)
+        return callback(e);
+    }
 };
