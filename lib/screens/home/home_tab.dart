49a758199f694b3f7df37bc89f1158bc46203dde
Christopher Butler <chris@project9.studio>
2022-07-26 17:18:09 -0400

    Added home tab

---
+++ lib/screens/home/home_tab.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text(
+            'Upcoming Events',
+            style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Gothic A1',
+                fontWeight: FontWeight.w700,
+                fontSize: 16),
+          )
+        ],
+      ),
+    );
+  }
+}
